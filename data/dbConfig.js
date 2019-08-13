fd048219fa082183a9672beba4a81a604d19ff4b
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-08-13 05:51:00 -0500

    Added accounts api

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -3,6 +3,3 @@ const knex = require('knex');
 const configOptions = require('../knexfile').development;
 
 module.exports = knex(configOptions);
-let yeet = "12";
-parseInt(yeet);
-console.log(yeet)
